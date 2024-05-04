705f5cddaab072bfce19694a8e03df50598bb533
twallie <thomaswallsmith@gmail.com>
2024-05-04 13:55:40 -0400

    checkerboard bits :)

---
+++ src/board.rs
@@ -0,0 +1,19 @@
+pub struct Board {
+    bytes: u64
+}
+
+impl Board {
+    pub fn new() -> Board {
+        return Board {
+            bytes: 0b0
+        }
+    }
+
+    pub fn set_with_offset(&mut self, offset: usize) {
+        self.bytes |= (0b1 << offset);
+    }
+
+    pub fn print_bytes(&self) {
+        println!("{:064b}", self.bytes); 
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,45 +1,17 @@
+use board::Board;
+
+mod board;
+
 fn main() {
     let mut board = Board::new();
-    board.set(1, 0);
-    board.print();
-
-    println!("0,0: {:?}", board.is_set(0, 0));
-    println!("0,1: {:?}", board.is_set(0, 1));
-}
-
-struct Board {
-    bytes: u64,
-}
-
-impl Board {
-    fn new() -> Self {
-        return Board {
-            bytes: 0b0,
-        }
-    }
     
-    fn print(&self) -> () {
-        println!("");
-        for row in 0..8 {
-            for col in 0..8 {
-                print!("{}", if self.is_set(7 - row, col) { "1" } else { "0" }); 
-            }
-            println!("");
+    for i in 0..64 {
+        if i % 2 == 1 {
+            continue;
         }
-        println!("");
+
+        board.set_with_offset(i);
     }
 
-    fn set(&mut self, row: usize, col: usize) {
-        let offset = Board::calculate_offset(row, col);
-        self.bytes |= 1 << offset;
-    }
-
-    fn is_set(&self, row: usize, col: usize) -> bool {
-        let offset = Board::calculate_offset(row, col);
-        return self.bytes & (1 << offset) == 1;
-    }
-
-    fn calculate_offset(row: usize, col: usize) -> usize {
-        return col + row;
-    }
+    board.print_bytes();
 }
