b7577c799344effecb8e6d5b963631563f8dd8cd
twallie <thomaswallsmith@gmail.com>
2024-05-08 20:47:27 -0700

    feat: basic life game instantiation

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,9 @@
-use board::Board;
-use board::Direction::{UP, DOWN, LEFT, RIGHT};
-
 mod board;
 mod errors;
+mod life;
 
+use crate::life::Life;
 
 fn main() {
-    let mut board = Board::new();
-
-    board.set_coordinate(1,1).unwrap();
-    board.set_coordinate(1,2).unwrap();
-    board.set_coordinate(0,1).unwrap();
-
-    println!("\n{}\n", board.is_coordinate_set(0,1).unwrap());
-    board.print_rows();
-
-    println!(
-        "\nUP: {}\nDOWN: {}\nRIGHT: {}\nLEFT: {}",
-        board.is_neighbor_set(1,1, UP).unwrap(),
-        board.is_neighbor_set(1,1, DOWN).unwrap(),
-        board.is_neighbor_set(1,1, RIGHT).unwrap(),
-        board.is_neighbor_set(1,1, LEFT).unwrap(),
-    ); 
-
-    board.unset_coordinate(0,1).unwrap();
-
-    println!("\n{}\n", board.is_coordinate_set(0,1).unwrap());
-    board.print_rows();
-
-    println!(
-        "\nUP: {}\nDOWN: {}\nRIGHT: {}\nLEFT: {}",
-        board.is_neighbor_set(1,1, UP).unwrap(),
-        board.is_neighbor_set(1,1, DOWN).unwrap(),
-        board.is_neighbor_set(1,1, RIGHT).unwrap(),
-        board.is_neighbor_set(1,1, LEFT).unwrap(),
-    ); 
+    let life = Life::new();
 }
