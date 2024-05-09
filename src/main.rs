29f005bb09db5489d82424bac968a3b6e0ea9ee7
twallie <thomaswallsmith@gmail.com>
2024-05-08 22:25:12 -0700

    feat: added some little examples

--- src/main.rs
+++ src/main.rs
@@ -9,19 +9,32 @@ use crate::life::Life;
 fn main() {
     let mut life = Life::new();
 
-    life.set_cell(3, 3).unwrap();
-    life.set_cell(4, 3).unwrap();
-    life.set_cell(2, 3).unwrap();
+    // Glider
+    life.set_cell(2, 7).unwrap();
+    life.set_cell(3, 6).unwrap();
+    life.set_cell(1, 5).unwrap();
+    life.set_cell(2, 5).unwrap();
+    life.set_cell(3, 5).unwrap();
 
     life.print_cells();
 
-    for _ in 0..10 {
+    for _ in 0..9999 {
         print!("\x1B[2J");
-        let ten_millis = time::Duration::from_millis(1000);
+        let ten_millis = time::Duration::from_millis(700);
         thread::sleep(ten_millis);
 
         life.next();
-        println!("");
         life.print_cells();
     }
 }
+
+/*
+DEMO THINGS
+    # Bee-Hive
+    life.set_cell(2, 3).unwrap();
+    life.set_cell(3, 3).unwrap();
+    life.set_cell(1, 2).unwrap();
+    life.set_cell(2, 1).unwrap();
+    life.set_cell(3, 1).unwrap();
+    life.set_cell(4, 2).unwrap();
+*/
